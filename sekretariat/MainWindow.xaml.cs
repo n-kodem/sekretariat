374565d828f981a6dd57957481d6faea8e9ccdc5
Nikodem Reszka <61233315+n-kodem@users.noreply.github.com>
2021-12-26 13:50:10 +0100

    Dodaj pliki projektów.

---
+++ sekretariat/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sekretariat
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void typeChange(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show("", comboBox.SelectedValue.ToString(), System.Windows.MessageBoxButton.OK);
+        }
+
+        private void onSubmit(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
