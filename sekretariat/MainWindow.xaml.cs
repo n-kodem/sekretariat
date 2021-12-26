37444fbef046ee2bf71b078c5728422c5b7a64b4
Nikodem Reszka <61233315+n-kodem@users.noreply.github.com>
2021-12-26 22:59:25 +0100

    v0.0.2 Nice gui, "Finished" person add panel

--- sekretariat/MainWindow.xaml.cs
+++ sekretariat/MainWindow.xaml.cs
@@ -23,16 +23,112 @@ namespace sekretariat
     /// 
     /// 
     /// v0.0.1
+
+
     public partial class MainWindow : Window
     {
+        private void hidePrompts()
+        {
+            foreach (var element in container.Children)
+            {
+                if (element.GetType() == typeof(TextBox))
+                {
+                    TextBox e = (TextBox)element;
+                    e.Focusable = false;
+                    e.Visibility = Visibility.Hidden;
+                }
+                else if (element.GetType() == typeof(DatePicker))
+                {
+                    DatePicker e = (DatePicker)element;
+                    e.Focusable = false;
+                    e.Visibility = Visibility.Hidden;
+
+                }
+                else if (element.GetType() == typeof(ComboBox))
+                {
+                    ComboBox e = (ComboBox)element;
+                    if (e.Name.ToString() != "typeSelector")
+                    {
+                        e.Focusable = false;
+                        e.Visibility = Visibility.Hidden;
+                    }
+                }
+            }
+        }
+        private void ShowBoxes(String arg)
+        {
+            imie.Focusable = true;
+            imie.Visibility = Visibility.Visible;
+
+            druImie.Focusable = true;
+            druImie.Visibility = Visibility.Visible;
+
+            nazwisko.Focusable = true;
+            nazwisko.Visibility = Visibility.Visible;
+
+            panNazwisko.Focusable = true;
+            panNazwisko.Visibility = Visibility.Visible;
+
+            imRodzicow.Focusable = true;
+            imRodzicow.Visibility = Visibility.Visible;
+
+            zdjecie.Focusable = true;
+            zdjecie.Visibility = Visibility.Visible;
+
+            plec.Focusable = true;
+            plec.Visibility = Visibility.Visible;
+
+            pesel.Focusable = true;
+            pesel.Visibility = Visibility.Visible;
+
+            dataUr.Focusable = true;
+            dataUr.Visibility = Visibility.Visible;
+
+            if (arg == "Pracownik")
+            {
+                etat.Focusable = true;
+                etat.Visibility = Visibility.Visible;
+
+                opisStan.Focusable = true;
+                opisStan.Visibility = Visibility.Visible;
+
+                dataZat.Focusable = true;
+                dataZat.Visibility = Visibility.Visible;
+            }
+            else if (arg == "Nauczyciel")
+            {
+                wychowawstwo.Focusable = true;
+                wychowawstwo.Visibility = Visibility.Visible;
+
+                przedmiotyNau.Focusable = true;
+                przedmiotyNau.Visibility = Visibility.Visible;
+                
+                klasyZGodz.Focusable = true;
+                klasyZGodz.Visibility = Visibility.Visible;
+
+                dataZat.Focusable = true;
+                dataZat.Visibility = Visibility.Visible;
+            }
+            else if (arg == "Uczeń")
+            {
+                klasa.Focusable = true;
+                klasa.Visibility = Visibility.Visible;
+
+                grupyJez.Focusable = true;
+                grupyJez.Visibility = Visibility.Visible;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            hidePrompts();
         }
 
         private void typeChange(object sender, SelectionChangedEventArgs e)
         {
-            MessageBox.Show("", comboBox.SelectedValue.ToString(), System.Windows.MessageBoxButton.OK);
+            hidePrompts();
+            ShowBoxes(typeSelector.SelectedValue.ToString());
         }
 
         private void onSubmit(object sender, RoutedEventArgs e)
@@ -43,6 +139,11 @@ namespace sekretariat
         private void CommonCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
+        }
+
+        private void loseFocus(object sender, MouseButtonEventArgs e)
+        {
+            Keyboard.ClearFocus();
         }
     }
 }
