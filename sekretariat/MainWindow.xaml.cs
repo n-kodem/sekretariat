a7ebc1b92c3e163e37c72525008964d00f35c1a9
Nikodem Reszka <61233315+n-kodem@users.noreply.github.com>
2021-12-28 03:08:13 +0100

    v0.0.4 - added Workers table view and now you can load images

--- sekretariat/MainWindow.xaml.cs
+++ sekretariat/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Data.Sqlite;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +14,11 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MaterialDesignThemes.Wpf;
 using static sekretariat.selectorDialog;
+using System.IO;
+using System.Data.SQLite;
+using System.Globalization;
+using Microsoft.Win32;
+using System.Drawing.Imaging;
 
 namespace sekretariat
 {
@@ -24,11 +28,64 @@ namespace sekretariat
     ///
     /// 
     /// 
-    /// v0.0.3
+    /// v0.0.4
 
 
     public partial class MainWindow : Window
     {
+        public string PathImageToBase64(string path)
+        {
+            System.Drawing.Image image = System.Drawing.Image.FromFile(path);
+
+            ImageFormat format = new ImageFormat(image.RawFormat.Guid);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+        public BitmapImage Base64ToWPFImageSource(string base64String)
+        {
+            // Convert Base64 String to byte[]
+            MessageBox.Show(base64String.ToString(), "XD?");
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+
+            MemoryStream ms = new MemoryStream(imageBytes, 0,
+              imageBytes.Length);
+
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+
+            bi.StreamSource = new MemoryStream(imageBytes);
+            bi.EndInit();
+
+            return bi;
+        }
+        public class WorkerRow
+        {
+            public string imie { set; get; }
+            public string druImie { set; get; }
+            public string nazwisko { set; get; }
+            public string panNazwisko { set; get; }
+            public string imRodzicow { set; get; }
+            public BitmapImage zdjecie { set; get; }
+            public string plec { set; get; }
+            public string pesel { set; get; }
+            public DateTime dataUr { set; get; }
+            public DateTime dataZat { set; get; }
+            public string opisStan { set; get; }
+            public string etat { set; get; }
+
+        }
         private void hidePrompts()
         {
             foreach (var element in container.Children)
@@ -99,6 +156,9 @@ namespace sekretariat
             submitBtn.Focusable = true;
             submitBtn.Visibility = Visibility.Visible;
 
+            photoBtn.Focusable = true;
+            photoBtn.Visibility = Visibility.Visible;
+
             if (arg == "Pracownik")
             {
                 etat.Focusable = true;
@@ -138,8 +198,8 @@ namespace sekretariat
         {
             string query = $"INSERT INTO {typeSelector.SelectedValue} ";
 
-            string rows = "(";
-            string values = "(";
+            string rows = "";
+            string values = "";
 
             foreach (var element in container.Children)
             {
@@ -149,7 +209,10 @@ namespace sekretariat
                     if (e.Text != "")
                     {
                         rows += $"{e.Name},";
-                        values+= $"{e.Text},";
+                        if(e.Name=="zdjecie")
+                            values += $"\"{PathImageToBase64(e.Text)}\",";
+                        else
+                            values += $"\"{e.Text}\",";
                     }
                         
                 }
@@ -161,7 +224,7 @@ namespace sekretariat
                     if (e.SelectedDate.Value.ToString() != "")
                     {
                         rows += $"{e.Name},";
-                        values+=$"{e.SelectedDate.Value.ToString()},";
+                        values+=$"\"{e.SelectedDate.Value}\",";
                     }
                        
 
@@ -173,17 +236,14 @@ namespace sekretariat
                     if (e.Name.ToString() != "typeSelector" && e.SelectedValue.ToString()!="")
                     {
                         rows += $"{e.Name},";
-                        values += $"{e.SelectedValue},";
+                        values += $"\"{e.SelectedValue}\",";
                     }
                 }
             }
             rows = rows.Remove(rows.Length - 1);
             values = values.Remove(values.Length - 1);
 
-            rows += ")";
-            values += ")";
-
-            query += $"{rows} VALUES {values};";
+            query += $"({rows}) VALUES ({values});";
             return query;
         }
 
@@ -191,6 +251,64 @@ namespace sekretariat
         {
             InitializeComponent();
             hidePrompts();
+            SQLiteConnection sqlite_conn;
+            SQLiteCommand sqlite_cmd;
+            // Create DB if not exists
+            bool newdb = File.Exists("school.db");
+            if (!newdb)
+            {
+                sqlite_conn = new SQLiteConnection("Data Source=school.db;Version=3;New=True;Compress=True;");
+                sqlite_conn.Open();
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                // TODO: Make this create tables: (Uczniowie, Nauczyciele, Pracownicy)
+                sqlite_cmd.CommandText = $"CREATE TABLE IF NOT EXISTS Pracownik (imie TEXT, druImie TEXT, nazwisko TEXT, panNazwisko Text, imRodzicow TEXT, zdjecie TEXT, plec TEXT, pesel TEXT, dataUr TEXT, dataZat TEXT, opisStan TEXT, etat TEXT);";
+                sqlite_cmd.ExecuteNonQuery();
+                sqlite_cmd.Reset();
+                sqlite_cmd.CommandText = $"CREATE TABLE IF NOT EXISTS Nauczyciel (imie TEXT, druImie TEXT, nazwisko TEXT, panNazwisko Text, imRodzicow TEXT, zdjecie TEXT, plec TEXT, pesel TEXT, dataUr TEXT, dataZat TEXT, przedmiotyNau TEXT, wychowawstwo TEXT, klasyZGodz TEXT);";
+                sqlite_cmd.ExecuteNonQuery();
+                sqlite_cmd.Reset();
+                sqlite_cmd.CommandText = $"CREATE TABLE IF NOT EXISTS Uczen (imie TEXT, druImie TEXT, nazwisko TEXT, panNazwisko Text, imRodzicow TEXT, zdjecie TEXT, plec TEXT, pesel TEXT, dataUr TEXT, klasa TEXT, grupyJez TEXT);";
+                sqlite_cmd.ExecuteNonQuery();
+                sqlite_cmd.Reset();
+                sqlite_conn.Close();
+                //MessageBox.Show("DB created");
+            }
+
+            sqlite_conn = new SQLiteConnection("DataSource=school.db;Version=3;");
+            sqlite_conn.Open();
+            sqlite_cmd = sqlite_conn.CreateCommand();
+            
+            // new data insertion (Automated)
+            sqlite_cmd.CommandText = "SELECT * FROM Pracownik;";
+            var read = sqlite_cmd.ExecuteReader();
+            List<WorkerRow> workers = new List<WorkerRow> { };
+            
+            while (read.Read())
+            {
+                
+                workers.Add(new WorkerRow
+                {
+                    imie = read.GetValue(read.GetOrdinal("imie")).ToString(),
+                    druImie = read.GetValue(read.GetOrdinal("druImie")).ToString(),
+                    nazwisko = read.GetValue(read.GetOrdinal("nazwisko")).ToString(),
+                    panNazwisko = read.GetValue(read.GetOrdinal("panNazwisko")).ToString(),
+                    imRodzicow = read.GetValue(read.GetOrdinal("imRodzicow")).ToString(),
+                    zdjecie = Base64ToWPFImageSource(read.GetValue(read.GetOrdinal("zdjecie")).ToString()),
+                    plec = read.GetValue(read.GetOrdinal("plec")).ToString(),
+                    pesel = read.GetValue(read.GetOrdinal("pesel")).ToString(),
+                    dataUr = DateTime.Parse(read.GetValue(read.GetOrdinal("dataUr")).ToString()),
+                    dataZat = DateTime.Parse(read.GetValue(read.GetOrdinal("dataZat")).ToString()),
+                    opisStan = read.GetValue(read.GetOrdinal("opisStan")).ToString(),
+                    etat = read.GetValue(read.GetOrdinal("etat")).ToString()
+                });
+                
+
+            }
+            sqlite_conn.Close();
+            workersTable.Items.Clear();
+            workersTable.ItemsSource = workers;
+            //workersTable.Items.Add(testc);
+            //workersTable.Items.Add(new WorkerRow {imie="Nikodem",druImie="Rafał",nazwisko="Reszka",panNazwisko="",imRodzicow="",zdjecie=new Image(),plec="",pesel="",dataUr=new DateTime(),dataZat=new DateTime(),opisStan="",etat=""});
         }
 
         private void typeChange(object sender, SelectionChangedEventArgs e)
@@ -199,17 +317,30 @@ namespace sekretariat
                 hidePrompts();
                 ShowBoxes(typeSelector.SelectedValue.ToString());
             }
-
-
-            
         }
 
         private void onSubmit(object sender, RoutedEventArgs e)
         {
             // TODO: Add validate function 
             MessageBox.Show(generateInsertQuery(), "");
+
+
+            SQLiteConnection sqlite_conn;
+            SQLiteCommand sqlite_cmd;
+
+            sqlite_conn = new SQLiteConnection("DataSource=school.db;Version=3;");
+            sqlite_conn.Open();
+            sqlite_cmd = sqlite_conn.CreateCommand();
+
+            // new data insertion (Automated)
+            sqlite_cmd.CommandText = generateInsertQuery();
+
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_conn.Close();
+
             typeSelector.SelectedIndex = -1;
             hidePrompts();
+
         }
         private void CommonCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
@@ -219,6 +350,36 @@ namespace sekretariat
         private void loseFocus(object sender, MouseButtonEventArgs e)
         {
             Keyboard.ClearFocus();
+        }
+
+        private void loadImgPath(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "";
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            string sep = string.Empty;
+
+            foreach (var c in codecs)
+            {
+                string codecName = c.CodecName.Substring(8).Replace("Codec", "Files").Trim();
+                dlg.Filter = String.Format("{0}{1}{2} ({3})|{3}", dlg.Filter, sep, codecName, c.FilenameExtension);
+                sep = "|";
+            }
+
+            dlg.Filter = String.Format("{0}{1}{2} ({3})|{3}", dlg.Filter, sep, "All Files", "*.*");
+
+            dlg.DefaultExt = ".png"; // Default file extension 
+
+            // Show open file dialog box 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results 
+            if (result == true)
+            {
+                // Open document 
+                zdjecie.Text = dlg.FileName;
+            }
         }
     }
 }
